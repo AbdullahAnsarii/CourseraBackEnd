92ceda672302d7ab07b616a6c5e6694170b74c4f
Abdullah Ansari <aa9272137@gmail.com>
2020-11-21 22:46:44 +0500

    user model created

---
+++ models/users.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+let schema = mongoose.Schema;
+
+let user = new schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('User', user);
