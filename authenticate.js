b17d487767c48d11b8d63746bd638cd2f45e65b6
Abdullah Ansari <aa9272137@gmail.com>
2020-11-29 19:35:11 +0500

    authenticate updated for jwt

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,38 @@
 let passport = require('passport');
 let localStrategy = require('passport-local').Strategy;
 let User = require('./models/user');
+let JwtStrategy = require('passport-jwt').Strategy;
+let ExtractJwt = require('passport-jwt').ExtractJwt;
+let jwt = require('jsonwebtoken'); //use to verify sign and create tokens
+let config = require('./config');
+const user = require('./models/user');
 
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function(user){
+    return jwt.sign(user, config.secretkey, {expiresIn: 3600});
+}
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretkey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts,
+    (jwt_payload, done) => {
+        console.log("JWT Payload: " + jwt_payload);
+        user.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err){
+                return done(err, false);
+            }
+            else if (user){
+                return done(null, true);
+            }
+            else{
+                return done(null, false);
+            }
+        })
+    }))
+   
+exports.verifyUser = passport.authenticate('jwt', {session: false});
